2b25e70034d5d97352c489bb6415476a9ed01a79
wisdomdaoleo <120478814liulei@gmail.com>
2022-03-26 23:08:10 +0800

    minterdapp_polygon_frontend

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x60cEE9BF9De8B988f43B677E7121d104D5A1eA4C"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
