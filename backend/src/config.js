d1a281b2aa6e496d07a975a472d57fa07c4ceb8b
wisdomdaoleo <120478814liulei@gmail.com>
2022-03-26 21:39:58 +0800

    minterdapp_polygon3

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,7 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 10,
+    growEditionSizeTo: 20,
     layersOrder: [
       { name: "Background" },
       { name: "Clothes" },
